10525c33ddf120cace3ba9c11f567935a5f1d41d
dimitrievdd <dimitrievdmit@yandex.ru>
2026-04-23 22:39:50 +0300

    feat. Добавлены юнит тесты работы с сериализацией dto в stat-dto.

---
+++ stat/stat-dto/src/test/java/ru/practicum/explorewithme/stats/dto/ViewStatsDTOJsonTest.java
@@ -0,0 +1,40 @@
+package ru.practicum.explorewithme.stats.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class ViewStatsDTOJsonTest {
+
+    @Autowired
+    private JacksonTester<ViewStatsDTO> jacksonTester;
+
+    @Test
+    void testSerialize() throws Exception {
+        var dto = new ViewStatsDTO("ewm-main-service", "/events", 42L);
+        var json = jacksonTester.write(dto);
+        assertThat(json).hasJsonPathStringValue("$.app");
+        assertThat(json).hasJsonPathStringValue("$.uri");
+        assertThat(json).hasJsonPathNumberValue("$.hits");
+        assertThat(json).extractingJsonPathNumberValue("$.hits").isEqualTo(42);
+    }
+
+    @Test
+    void testDeserialize() throws Exception {
+        String content = """
+                {
+                    "app": "app1",
+                    "uri": "/uri",
+                    "hits": 7
+                }
+                """;
+        var dto = jacksonTester.parse(content).getObject();
+        assertThat(dto.getApp()).isEqualTo("app1");
+        assertThat(dto.getUri()).isEqualTo("/uri");
+        assertThat(dto.getHits()).isEqualTo(7L);
+    }
+}
