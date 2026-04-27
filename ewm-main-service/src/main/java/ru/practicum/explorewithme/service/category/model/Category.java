7caa24543c6f13636b43f9250c765de3a9be7977
Inessa Babenko <iness.tt.eln@gmail.com>
2026-04-27 14:12:38 +0700

    add: добавлена модель категории и запроса новой категории

---
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/service/category/model/Category.java
@@ -0,0 +1,27 @@
+package ru.practicum.explorewithme.service.category.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 50)
+    @NotBlank
+    @Column(unique = true)
+    private String name;
+}
