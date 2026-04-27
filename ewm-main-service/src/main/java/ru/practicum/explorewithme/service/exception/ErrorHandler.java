8e6f5fec24262f759f12a4d0683abefa3d18964c
Inessa Babenko <iness.tt.eln@gmail.com>
2026-04-27 14:11:25 +0700

    add: добавлен exceptionHandler, DuplicatedDataException, ExceptionResponse

---
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/service/exception/ErrorHandler.java
@@ -0,0 +1,16 @@
+package ru.practicum.explorewithme.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(DuplicatedDataException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleDuplicatedDataException(final DuplicatedDataException e) {
+        return new ExceptionResponse("Неверные данные", e.getMessage());
+    }
+}
---
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/service/exception/ExceptionResponse.java
@@ -0,0 +1,4 @@
+package ru.practicum.explorewithme.service.exception;
+
+public record ExceptionResponse(String error, String description) {
+}
