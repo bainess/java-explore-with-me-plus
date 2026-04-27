0bc5c2ae4406e2c4822448af562998bd9cb21b44
Inessa Babenko <iness.tt.eln@gmail.com>
2026-04-27 15:24:40 +0700

    refactor: добавлен final для CategoryAdminController поля

--- ewm-main-service/src/main/java/ru/practicum/explorewithme/service/category/controller/CategoryAdminController.java
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/service/category/controller/CategoryAdminController.java
@@ -2,10 +2,8 @@ package ru.practicum.explorewithme.service.category.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.explorewithme.service.category.dto.NewCategoryRequest;
 import ru.practicum.explorewithme.service.category.model.Category;
 import ru.practicum.explorewithme.service.category.service.CategoryService;
@@ -15,9 +13,10 @@ import ru.practicum.explorewithme.service.category.service.CategoryService;
 @RequiredArgsConstructor
 @RequestMapping("/admin/categories")
 public class CategoryAdminController {
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Category createCategory(@RequestBody NewCategoryRequest request) {
         log.info("Получен запрос на создание категории {}", request.getName());
 
