63fb5aa8b01394450ffda5e88ee6d906f4bebdd1
Inessa Babenko <iness.tt.eln@gmail.com>
2026-05-06 21:08:58 +0700

    fix: возвращаемый тип изменен на Integer для совместимости с репозиторием

--- ewm-main-service/src/main/java/ru/practicum/explorewithme/service/config/AppProperties.java
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/service/config/AppProperties.java
@@ -1,8 +1,6 @@
 package ru.practicum.explorewithme.service.config;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
