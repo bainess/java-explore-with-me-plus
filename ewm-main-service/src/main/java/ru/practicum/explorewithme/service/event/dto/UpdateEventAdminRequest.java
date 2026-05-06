6ce703711bf1a7fc03a8f50665adeecf1a218d9c
Inessa Babenko <iness.tt.eln@gmail.com>
2026-05-06 20:50:31 +0700

    fix: возвращаемый тип изменен на Integer для совместимости с репозиторием

--- ewm-main-service/src/main/java/ru/practicum/explorewithme/service/event/dto/UpdateEventAdminRequest.java
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/service/event/dto/UpdateEventAdminRequest.java
@@ -27,7 +27,7 @@ public class UpdateEventAdminRequest {
     Boolean paid;
 
     @PositiveOrZero(message = "Лимит участников не может быть отрицательным")
-    private int participantLimit;
+    private Integer participantLimit;
 
     Boolean requestModeration;
 
