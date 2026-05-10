e8805399153d0e43599c829baf6f87f857bf62ae
Inessa Babenko <iness.tt.eln@gmail.com>
2026-05-10 20:03:17 +0700

    fix: checkstyle

--- ewm-main-service/src/main/java/ru/practicum/explorewithme/service/location/dal/LocationRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/service/location/dal/LocationRepository.java
@@ -1,11 +1,7 @@
 package ru.practicum.explorewithme.service.location.dal;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.explorewithme.service.location.model.Location;
-
-import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
     boolean existsByName(String name);
