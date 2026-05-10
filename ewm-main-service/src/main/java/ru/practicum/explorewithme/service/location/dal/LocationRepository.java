66997a7be4e60c081d5b67d1ef24f2632b0fb20b
Inessa Babenko <iness.tt.eln@gmail.com>
2026-05-10 13:00:05 +0700

    add: подтверждение существования названия в бд

--- ewm-main-service/src/main/java/ru/practicum/explorewithme/service/location/dal/LocationRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/service/location/dal/LocationRepository.java
@@ -1,5 +1,7 @@
 package ru.practicum.explorewithme.service.location.dal;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.explorewithme.service.location.model.Location;
 
@@ -9,4 +11,6 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
     boolean existsByName(String name);
 
     Boolean existsByNameIgnoreCaseAndIdNot(String name, Long id);
+
+    Boolean existsByNameIgnoreCase(String name);
 }
